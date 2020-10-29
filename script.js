5debee08f99c1a0830f8e5ce642f11a4dfaa8575
Macs <72122606+macs023@users.noreply.github.com>
2020-10-29 08:24:33 +0530

    This change is cool

--- script.js
+++ script.js
@@ -27,12 +27,15 @@ function setTheme(mode) {
         document.getElementById('theme-style').href = 'blue.css'
     }
 
-    if (mode == 'green') {
-        document.getElementById('theme-style').href = 'green.css'
-    }
+    // if (mode == 'green') {
+    //     document.getElementById('theme-style').href = 'green.css'
+    // }
 
-    if (mode == 'purple') {
-        document.getElementById('theme-style').href = 'purple.css'
-    }
+    // if (mode == 'purple') {
+    //     document.getElementById('theme-style').href = 'purple.css'
+    // }
     localStorage.setItem('theme', mode)
 }
+
+
+
